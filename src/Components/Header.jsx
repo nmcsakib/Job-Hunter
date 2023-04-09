54d64497a35631572ffc115b21ff20e4fb18aafd
Sakib <bksumon51@gmail.com>
2023-04-10 05:13:18 +0600

    added banner text and image

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li className='bg-gradient-to-r from-[#9873FF] to-[#7E90FE]  bg-clip-text text-transparent'><a>Statistic</a></li>
+        <li className='gradient-text'><a>Statistic</a></li>
         <li tabIndex={0}>
           <a className="justify-between">
             Applied jobs
@@ -36,7 +36,7 @@ const Header = () => {
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn border-none bg-gradient-to-r from-[#9873FF] to-[#7E90FE] ">Start Applying</a>
+    <a className="my-btn">Start Applying</a>
   </div>
 </div>
         </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            home
+        <div className='font-manrope'>
+            <div className="banner px-10 mx-auto flex justify-between items-center my-10">
+                <div className="flex flex-col gap-4">
+                <h1 className="font-bold text-7xl leading-tight">
+                    One step <br /> closer to your <br /><span className='gradient-text'>Drem Job</span>
+                    </h1>
+                    <p className="tracking-wide font-light text-lg">Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                    <button className="my-btn w-1/2">Get started</button>
+                </div>
+                <img src="resources/Images/banner-person.png" alt="" />
+            </div>
         </div>
     );
 };
